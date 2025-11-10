a471fab61ea5b75504beb0a01493bfee17c7ad25
herick3137 <klannlimaherickfilipe@gmail.com>
2025-11-10 16:48:00 -0300

    first commit

---
+++ estufa/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Leitura, Alerta
+from .views import LeituraViewSet, AlertaViewSet
+
+@admin.register(Leitura)
+class LeituraAdmin(admin.ModelAdmin):
+    list_display = ('temperatura', 'umidade', 'data_hora')
+    list_filter = ('data_hora',)
+    ordering = ('-data_hora',)
+
+@admin.register(Alerta)
+class AlertaAdmin(admin.ModelAdmin):
+    list_display = ('tipo', 'mensagem', 'lido', 'data_hora')
+    list_filter = ('tipo', 'lido', 'data_hora')
+    ordering= ('-data_hora',)
+ 
